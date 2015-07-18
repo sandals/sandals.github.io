34a12d9c2fc9ff9cc57877c1711de1b871f90cdc
Brandon Newton <sandalsboy@gmail.com>
2015-07-17 22:12:13 -0700

    initial commit

---
+++ config.rb
@@ -0,0 +1,11 @@
+project_type = :stand_alone
+http_path = '/'
+sass_dir = 'scss'
+css_dir = 'css'
+images_dir = 'img'
+fonts_dir = 'fonts'
+javascripts_dir = 'js'
+line_comments = false
+preferred_syntax = :scss
+output_style = :compressed
+relative_assets = true
